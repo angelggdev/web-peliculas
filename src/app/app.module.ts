898ca9f0be9ad1c083bc83315f542038a33d5756
Angel <angelggdev@gmail.com>
2021-12-31 11:02:28 -0300

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieComponent } from './movie/movie.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { BillboardComponent } from './billboard/billboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MovieListComponent,
+    MovieComponent,
+    MovieDetailComponent,
+    BillboardComponent
   ],
   imports: [
     BrowserModule,
