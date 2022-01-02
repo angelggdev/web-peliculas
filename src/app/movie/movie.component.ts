759a2c5166be8b5cd9f69fa47ed569c47b3c1ec5
Angel <angelggdev@gmail.com>
2022-01-02 12:33:26 -0300

    function to display rating of the movies

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -10,10 +10,29 @@ export class MovieComponent implements OnInit {
   @Input() posterPath:string;
   @Input() movieTitle:string;
   @Input() averageVote:number;
+  starsConfig: Array<string>;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.starsConfig = this.getStarsConfig(this.averageVote - 1);
+  }
+
+  getStarsConfig(number:number): Array<string>{
+    let starsArray:Array<string> = [];
+
+    for (let i = 0; i < 10; i++) {
+      
+      if ( number <= i-0.4 && number >= i-0.6){
+        starsArray.push('half-full')
+      } else if (i <= Math.round(number)){
+        starsArray.push('full');
+      } else {
+        starsArray.push('blank')
+      }
+      
+    }
+    return starsArray;
   }
 
 }
